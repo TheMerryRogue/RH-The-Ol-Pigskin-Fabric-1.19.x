e32942475b11aa548c2e74519c85fa8a87c6cbb6
Merry Rogue <themerryrogue@gmail.com>
2022-08-10 17:20:37 -0700

    Connecting GitHub to IntelliJ project

---
+++ src/main/java/net/merryrogue/rhtheolpigskin/TheOlPigskin.java
@@ -0,0 +1,17 @@
+package net.merryrogue.rhtheolpigskin;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TheOlPigskin implements ModInitializer
+{
+	public static final String MOD_ID = "rhtheolpigskin";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
---
+++ src/main/java/net/merryrogue/rhtheolpigskin/TheOlPigskinClient.java
@@ -0,0 +1,10 @@
+package net.merryrogue.rhtheolpigskin;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheOlPigskinClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
